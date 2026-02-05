bf09f57ee4612f7ee4a45ec8539315552fe86682
pradashades333 <alexdreve03@gmail.com>
2026-02-05 21:59:38 +0100

    added button that lets u change the total squares

--- index.js
+++ index.js
@@ -25,4 +25,34 @@ clearButton.addEventListener('click', function(){
     
 })
 
+
+const sizeButton = document.getElementById("size")
+
+sizeButton.addEventListener('click', function(){
+    let size = parseInt(prompt("Enter the cube size"));
+
+    container.innerHTML = '';
+
+    
+    let totalSquare = size * size;
+
+    let squareSize = 960 / size;
+
+
+    for (let i = 0; i < totalSquare; i++){
+    const square = document.createElement("div");
+    square.classList.add('square');
+    container.appendChild(square);
+
+    square.style.width  = squareSize + "px"
+    square.style.height = squareSize + "px"
+
+    square.addEventListener("mouseover", function() {
+        square.style.backgroundColor = "black"
+    })
+}
+
+ 
+})
+
 console.log(container)
